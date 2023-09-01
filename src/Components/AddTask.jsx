32ca77c0571a6bbb7f7ea3d453b9ff79ed0032d7
adithyan-sivaraman <adi.mrmark@gmail.com>
2023-09-01 19:53:59 +0530

    changes in Add Task

--- src/Components/AddTask.jsx
+++ src/Components/AddTask.jsx
@@ -2,11 +2,12 @@ import { useState } from "react";
 import apiEndpoint from '../config'
 import { useTaskContext } from '../Context';
 const AddTask = () => {
-    const [formData, setFormData] = useState({
+    const initialState = {
         "title": '',
         "date": '',
         "status": 'pending',
-    });
+    }
+    const [formData, setFormData] = useState(initialState);
 
     const { completeTask, updateTaskData,popUpVisible, setPopupVisible}  = useTaskContext();
     const [dialogVisible, setDialogVisible] = useState(false);
@@ -45,15 +46,16 @@ const AddTask = () => {
             });
 
             const responseData = await response.json();
-            if (response.status === 200) {
+            if (response.status === 200 && responseData) {
                 setDialogText("Task added successfully")
                 setDialogVisible(true);
                 updateTaskData('add',formData);
                 setTimeout(() => {
                     setDialogVisible(false);
                 }, 2000)
+            setFormData(initialState)
             }
-            console.log(responseData);
+    
         } catch (error) {
             console.error('Error:', error);
         }
@@ -65,7 +67,7 @@ const AddTask = () => {
             <p className="text-base sm:text-lg: lg:text-xl font-extrabold px-2 py-2 flex flex-row items-center"><span className="material-icons">add</span>Add a Task</p>
             <form className="px-2 w-full" onSubmit={handleSubmit}>
                 <label htmlFor="title" className="text-base capitalize font-bold sm:mt-4 block">Title</label>
-                <input
+                <textarea
                     onInput={handleInput}
                     value={formData.title}
                     type="text"
@@ -73,7 +75,7 @@ const AddTask = () => {
                     id="title"
                     placeholder="enter title"
                     className="text-xs sm:text-sm lg:text-base w-full border-2 p-2 mt-4"
-                    required />
+                    required ></textarea>
 
                 <label htmlFor="date" className="text-base capitalize font-bold sm:mt-4 block">Due Date</label>
                 <input
